f5c7cb2d783394ce96460bec2d84b49a6e39dd37
DennisNew1 <dennis.neumann@smail.th-koeln.de>
2025-06-27 12:41:50 +0200

    Add button and script

---
+++ Experiment/Assets/Experiment Assets/VRButton.cs
@@ -0,0 +1,21 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class VRButton : MonoBehaviour
+{
+
+    public void LoadHandControllScene()
+    {
+        // SceneManager.LoadScene();
+    }
+
+    public void LoadControllerScene()
+    {
+        // SceneManager.LoadScene();
+    }
+
+    public void LoadHybridScene()
+    {
+        // SceneManager.LoadScene();
+    }
+}
