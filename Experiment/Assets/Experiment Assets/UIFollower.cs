fb7aee9ebe50f1cf4d38015b9720c132f3190fe3
Ryuchard <116103923+Ryuchard@users.noreply.github.com>
2025-07-23 16:41:21 +0200

    update

--- Experiment/Assets/Experiment Assets/UIFollower.cs
+++ Experiment/Assets/Experiment Assets/UIFollower.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class UIFollower : MonoBehaviour
 {
     public Transform cameraTransform;
-    public Vector3 offset = new Vector3(0, -0.2f, 1.5f);
+    public Vector3 offset = new Vector3(0, 0.1f, 1.5f);
 
     void Update()
     {
