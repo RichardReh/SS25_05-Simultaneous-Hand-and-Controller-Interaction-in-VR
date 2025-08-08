44bb73bedfc170d139e33b09823cffe081b383c3
DennisNew1 <dennis.neumann@smail.th-koeln.de>
2025-08-08 13:00:14 +0200

    Add PodestButtonfeedback

--- Experiment/Assets/Experiment Assets/PodestButton.cs
+++ Experiment/Assets/Experiment Assets/PodestButton.cs
@@ -2,20 +2,24 @@ using UnityEngine;
 
 public class PodestButton : MonoBehaviour
 {
+
+    public Material InteractionMaterial;
+    public GameObject InteractionGO;
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Etwas berührt den Button: " + other.name + " | Tag: " + other.tag);
 
-            GameObject plane = GameObject.FindGameObjectWithTag("Plane");
-            Debug.Log("TRIGGERED KNOPF");
-            if (plane != null)
-            {
-                Debug.Log("PLANE IST NICHT NULL");
-                plane.GetComponent<PressurePlateRandomizer>()?.ResetAndRandomize();
-                plane.GetComponent<PressurePlateColorRandomizer>()?.RandomizeAll();
-                UICounterManager.Instance.Reset();
-                LogWriter.Instance.WriteToLog("PodestButton: Startbutton wurde gedruckt");
-            }
-        
+        GameObject plane = GameObject.FindGameObjectWithTag("Plane");
+        Debug.Log("TRIGGERED KNOPF");
+        if (plane != null)
+        {
+            Debug.Log("PLANE IST NICHT NULL");
+            plane.GetComponent<PressurePlateRandomizer>()?.ResetAndRandomize();
+            plane.GetComponent<PressurePlateColorRandomizer>()?.RandomizeAll();
+            UICounterManager.Instance.Reset();
+            InteractionGO.GetComponent<MeshRenderer>().material = InteractionMaterial;
+            LogWriter.Instance.WriteToLog("PodestButton: Startbutton wurde gedruckt");
+        }
+
     }
 }
